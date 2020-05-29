a12c7477c3dfd332185fee532a9070fb66cd35b6
brian <brian@mirian.tech>
2020-05-28 21:11:18 -0400

    update fabric set up script

--- Build Supply Chain DApps with Hyperledger Fabric/loadFabric.sh
+++ Build Supply Chain DApps with Hyperledger Fabric/loadFabric.sh
@@ -5,3 +5,6 @@
 # load fabric images 2.1.0 fabric-ca 1.4.7 fabic-couchdb 0.4.20
 curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.1.0 1.4.7 0.4.20
 echo "========= download completed =========== "
+cp -R fabric-samples/bin .
+cp -R fabric-samples/config .
+echo "========= completed config set up =========== "
